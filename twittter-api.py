7361ccaa6a48d00d414f2dd81f1ed8c52807ca92
datascientist-kenn <60251639+datascientist-kenn@users.noreply.github.com>
2020-03-24 11:10:44 +0100

    Update twittter-api.py

--- twittter-api.py
+++ twittter-api.py
@@ -7,10 +7,10 @@ import json
 twitter_cred = {}
 
 #Inputing my credential details
-    twitter_cred['CONSUMER_KEY'] = 'iGLGGPwbK5YP86U2eLhlm6DJS'
-    twitter_cred['CONSUMER_SECRET'] = 'RLRHaEVq5NHb3PhwNEr8yf4Y0QwcNZNmjgvk5WaBMo3SSkRQY8'
-    twitter_cred['ACCESS_KEY'] = '1220695170028621824-RI7ZQQCEpBuaz3rqlrsBN3TNxXmTOY'
-    twitter_cred['ACCESS_SECRET'] = 'A8a8QODsZeiWnhxaWNXImNL2imrh0VnXYXRhOI6Ud1vqk'
+    twitter_cred['CONSUMER_KEY'] = 'XXXXXXXXXXXXXXXXXXXXXXX'
+    twitter_cred['CONSUMER_SECRET'] = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+    twitter_cred['ACCESS_KEY'] = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+    twitter_cred['ACCESS_SECRET'] = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
 
 #Saving the information to a .json file for easy and safe re-use
 with open('twitter_credentials.json', 'w') as json_file:
